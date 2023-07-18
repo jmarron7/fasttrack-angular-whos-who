9d73ffd0d0f368c2a528f7920d29d30fc63a0e52
alesscif <alexciogli1@gmail.com>
2023-07-18 09:19:19 -0700

    added routing! /game now accessible

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { GameComponent } from './game/game.component';
@@ -11,7 +11,7 @@ const routes: Routes = [{ path: "", component: HomeComponent }];
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, GameComponent],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
